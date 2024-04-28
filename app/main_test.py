8edad20190b054a6cc07f65740ebcafac37ed143
papecode <papesaliouwade500@gmail.com>
2024-04-28 13:53:02 +0000

    first commit

---
+++ app/main_test.py
@@ -0,0 +1,18 @@
+from main import return_backwards_string, get_mode
+import unittest
+import os
+
+
+class TestMain(unittest.TestCase):
+    def test_return_backwards_string(self):
+        random_string = "Hello"
+        random_string_reversed = "olleH"
+        self.assertEqual(random_string_reversed,
+                        return_backwards_string(random_string))
+
+    def test_get_mode(self):
+        self.assertEqual(os.environ.get("MODE"), get_mode())
+
+
+if __name__ == "__main__":
+    unittest.main()
