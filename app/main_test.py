07fde1339e4e685d31f73561a1d6b67eb93a7e7c
papecode <papesaliouwade500@gmail.com>
2024-04-29 09:57:46 +0000

    Test workflows

--- app/main_test.py
+++ app/main_test.py
@@ -6,7 +6,7 @@ class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string = "Hello"
         random_string_reversed = "olleH"
-        self.assertEqual(random_string_reversed, return_backwards_string(random_string))
+        self.assertNotEqual(random_string_reversed, return_backwards_string(random_string))
 
     def test_get_mode(self):
         self.assertEqual(os.environ.get("MODE"), get_mode())
